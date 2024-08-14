ddc515ddacbfda295b591266d3fab4959498995d
ArjunSohur <arjunsohur@gmail.com>
2024-08-14 17:46:34 -0500

    algorithm/generation folder

--- hypogenic/algorithm/generation/default.py
+++ hypogenic/algorithm/generation/default.py
@@ -12,6 +12,10 @@ from ...prompt import BasePrompt
 
 @generation_register.register("default")
 class DefaultGeneration(Generation):
+    """
+    Add on extra functionality to the generation fucntion - we consider this 
+    the "default" task.
+    """
     def __init__(
         self,
         api,
@@ -19,46 +23,77 @@ class DefaultGeneration(Generation):
         inference_class: Inference,
         task: BaseTask,
     ):
+        """
+        Parameters:
+            api: the language model that you're using, which may or may not be local
+            prompt_class: let's us know how the prompt is going to look
+            inference_class: gives us a way to predict labels for accuracy sake
+            task: determines the goal to accomplish
+        """
         super().__init__(api, prompt_class, inference_class, task)
 
+    # ------------------------------------------------------------------------ #
+    #                                                                          #
+    # ------------------------------------------------------------------------ #
+    # INITLALIZE HYPOTHESES                                                    #
+    # ------------------------------------------------------------------------ #
+    #                                                                          #
+    # ------------------------------------------------------------------------ #
     def initialize_hypotheses(
         self, num_init, init_batch_size, init_hypotheses_per_batch, alpha, **kwargs
     ):
         """Initialization method for generating hypotheses. Make sure to only loop till args.num_init
 
         Parameters:
-        ____________
-
-        num_init:
-        init_batch_size:
-        init_hypotheses_per_batch:
-
-        ____________
+            num_init: the total amount of hypothesis you want to generate initalily
+            init_batch_size: The amount of samples you want to include in each call to "batched_hypothesis_generation"
+            init_hypotheses_per_batch: the amount of hypotheses that you're going to generate per batch
+            alpha: the exploration constant in the hypogenic reward function
 
         Returns:
-        ____________
-
-        hypotheses_bank: A  dictionary with keys as hypotheses and the values as the Summary Information class
+            hypotheses_bank: A  dictionary with keys as hypotheses and the values as the Summary Information class
         """
+
+        # ----------------------------------------------------------------------
+        # Finding batch size and confirming that it works
+        # ----------------------------------------------------------------------
         assert (
-            num_init % init_batch_size == 0
+            num_init % init_hypotheses_per_batch == 0
         ), "Number of initial examples must be divisible by the batch size"
-        num_batches = num_init // init_batch_size
+
+        # To know how many batches we need to get to our desiered number of hypotheses
+        num_batches = num_init // init_hypotheses_per_batch
         hypotheses_bank = {}
+
+        # ----------------------------------------------------------------------
+        # The hypothesis creation process
+        # ----------------------------------------------------------------------
         for i in range(num_batches):
+
+            # get the samples that you're going to use to generate the hypotheses
             example_indices = list(
                 range(i * init_batch_size, (i + 1) * init_batch_size)
             )
+
+            # the new_hypotheses 
             new_hypotheses = self.batched_hypothesis_generation(
                 example_indices,
                 len(example_indices),
                 init_hypotheses_per_batch,
                 alpha,
             )
+
             hypotheses_bank.update(new_hypotheses)
 
         return hypotheses_bank
 
+    # ------------------------------------------------------------------------ #
+    #                                                                          #
+    # ------------------------------------------------------------------------ #
+    # BATCH INITLALIZE HYPOTHESES                                              #
+    # ------------------------------------------------------------------------ #
+    #                                                                          #
+    # ------------------------------------------------------------------------ #
     def batched_initialize_hypotheses(
         self,
         num_init,
@@ -68,11 +103,27 @@ class DefaultGeneration(Generation):
         use_cache=1,
         **kwargs
     ):
+        """
+        Batches the process of making new hypotheses
+
+        Parameters:
+            num_init: the number of hypotheses that you want to create out of this function
+            init_batch size: the number of samples that will be used to generate these hypotheses
+            init_hypotheses_per_batch: the amount of hypotheses that you want to generate per btach
+            alpha: the exploration constant in the hypogenic reward function
+        """
+        # ----------------------------------------------------------------------
+        # Finding batch size and confirming that it works
+        # ----------------------------------------------------------------------
         assert (
-            num_init % init_batch_size == 0
+            num_init % init_hypotheses_per_batch == 0
         ), "Number of initial examples must be divisible by the batch size"
-        num_batches = num_init // init_batch_size
+        num_batches = num_init // init_hypotheses_per_batch
         prompt_inputs = []
+
+        # ----------------------------------------------------------------------
+        # Making the batch of the responses that will be used in the batch generation
+        # ----------------------------------------------------------------------
         for i in range(num_batches):
             example_indices = list(
                 range(i * init_batch_size, (i + 1) * init_batch_size)
@@ -88,6 +139,9 @@ class DefaultGeneration(Generation):
             )
         responses = self.api.batched_generate(prompt_inputs, use_cache=use_cache)
 
+        # ----------------------------------------------------------------------
+        # Makes all the desired hypotheses
+        # ----------------------------------------------------------------------
         return self.batched_batched_hypothesis_generation(
             list(range(num_init)),
             num_init,
--- hypogenic/algorithm/generation/utils.py
+++ hypogenic/algorithm/generation/utils.py
@@ -1,13 +1,26 @@
 def extract_hypotheses(text, num_hypotheses):
+    """
+    Given a response with hypotheses, we want to take all of them out from the text.
+    This function might need to be adjusted depending on the prompt and the
+    robust-ness of the LM
+
+    Parameters:
+        text: the text with the hypotheses enclosed
+        num_hypotheses: the number of hypotheses that you want
+    """
     import re
 
+    # The regex to extract the hypotheses - matches to a numbered list
     pattern = re.compile(r"\d+\.\s(.+?)(?=\d+\.\s|\Z)", re.DOTALL)
     print("Text provided", text)
     hypotheses = pattern.findall(text)
+
+    # Here, either there are no hypotheses, or you didn't match them correctly
     if len(hypotheses) == 0:
         print("No hypotheses are generated.")
         return []
 
+    # this is a bit sketchy 
     if len(hypotheses) != num_hypotheses:
         print(f"Expected {num_hypotheses} hypotheses, but got {len(hypotheses)}.")
 
