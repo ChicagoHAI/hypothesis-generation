2bd6aa9883a5d301c65e264dc1d2d5c74156a158
laoliu5280 <haokunliu@uchicago.edu>
2024-08-15 13:58:33 -0500

    revert previous mistake changing generation/default.py

--- hypogenic/algorithm/generation/default.py
+++ hypogenic/algorithm/generation/default.py
@@ -52,8 +52,8 @@ class DefaultGeneration(Generation):
         """Initialization method for generating hypotheses. Make sure to only loop till args.num_init
 
         Parameters:
-            num_init: the total amount of hypothesis you want to generate initalily
-            init_batch_size: The amount of samples you want to include in each call to "batched_hypothesis_generation"
+            num_init: the total amount of examples you want to use for initialize hypotheses
+            init_batch_size: The amount of examples you want to include in each call to "batched_hypothesis_generation"
             init_hypotheses_per_batch: the amount of hypotheses that you're going to generate per batch
             alpha: the exploration constant in the hypogenic reward function
             max_concurrent: the maximum amount of concurrent calls to the API
@@ -66,11 +66,11 @@ class DefaultGeneration(Generation):
         # Finding batch size and confirming that it works
         # ----------------------------------------------------------------------
         assert (
-            num_init % init_hypotheses_per_batch == 0
+            num_init % init_batch_size == 0
         ), "Number of initial examples must be divisible by the batch size"
 
         # To know how many batches we need to get to our desiered number of hypotheses
-        num_batches = num_init // init_hypotheses_per_batch
+        num_batches = num_init // init_batch_size
         hypotheses_bank = {}
 
         # ----------------------------------------------------------------------
@@ -78,7 +78,7 @@ class DefaultGeneration(Generation):
         # ----------------------------------------------------------------------
         for i in range(num_batches):
 
-            # get the samples that you're going to use to generate the hypotheses
+            # get the examples that you're going to use to generate the hypotheses
             example_indices = list(
                 range(i * init_batch_size, (i + 1) * init_batch_size)
             )
@@ -117,8 +117,8 @@ class DefaultGeneration(Generation):
         Batches the process of making new hypotheses
 
         Parameters:
-            num_init: the number of hypotheses that you want to create out of this function
-            init_batch size: the number of samples that will be used to generate these hypotheses
+            num_init: the total amount of examples you want to use for initialize hypotheses
+            init_batch size: the number of examples that will be used to generate these hypotheses
             init_hypotheses_per_batch: the amount of hypotheses that you want to generate per btach
             alpha: the exploration constant in the hypogenic reward function
             use_cache: whether or not you want to use the cache
@@ -128,9 +128,9 @@ class DefaultGeneration(Generation):
         # Finding batch size and confirming that it works
         # ----------------------------------------------------------------------
         assert (
-            num_init % init_hypotheses_per_batch == 0
+            num_init % init_batch_size == 0
         ), "Number of initial examples must be divisible by the batch size"
-        num_batches = num_init // init_hypotheses_per_batch
+        num_batches = num_init // init_batch_size
         prompt_inputs = []
 
         # ----------------------------------------------------------------------
