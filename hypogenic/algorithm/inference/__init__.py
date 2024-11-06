34459509a21cbe0e8c8a838bf99615c21e26e39e
laoliu5280 <haokunliu@uchicago.edu>
2024-11-06 10:45:21 -0600

    remove relevance import

--- hypogenic/algorithm/inference/__init__.py
+++ hypogenic/algorithm/inference/__init__.py
@@ -8,4 +8,3 @@ from .filter_and_weight import FilterAndWeightInference
 from .one_step_adaptive import OneStepAdaptiveInference
 from .two_step_adaptive import TwoStepAdaptiveInference
 from .upperbound import UpperboundInference
-from .relevance_inference import RelevanceInference
