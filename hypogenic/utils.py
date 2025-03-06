default extract label fix, remove adjust_label, fix f_1 score computation

hypogenic/utils.py
@@ -41,11 +41,11 @@ def get_results(pred_list, label_list):
     """
     Compute accuracy and F1 score for multi-class classification
     """
-    accuracy = accuracy_score(label_list, pred_list)
+    valid_labels = set(label_list)
+    accuracy = accuracy_score(label_list, pred_list, labels=list(valid_labels))
     f1 = f1_score(label_list, pred_list, average="macro")
-
+    
     return {"accuracy": accuracy, "f1": f1}
-
 
 def set_seed(seed):
     logger = LoggerConfig.get_logger(logger_name)
@@ -53,20 +53,6 @@ def set_seed(seed):
     random.seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
-
-def adjust_label(preds, labels):
-    preds_out = []
-    existing_labels = set(labels)
-    for i in range(len(labels)):
-        if preds[i] not in existing_labels:
-            for existing_label in existing_labels:
-                if labels[i] != existing_label:
-                    preds_out.append(existing_label)
-                    break
-        else:
-            preds_out.append(preds[i])
-
-    return preds_out
 
 def load_hypotheses(filename: str) -> Dict[str, SummaryInformation]:
     """Load hypotheses from a JSON file."""